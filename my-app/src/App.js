9c458d6bba467d2fcff736840a5bee34c2170a47
pankajpatidar43145 <93091123+pankajpatidar43145@users.noreply.github.com>
2023-06-25 23:32:19 +0530

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import './App.css';
+
+function LoginForm() {
+    return ( <
+        div className = "login-box" >
+        <
+        h2 > Login < /h2> <
+        form >
+        <
+        div className = "user-box" >
+        <
+        input type = "text"
+        name = ""
+        required className = "input-field" / >
+        <
+        label > Username < /label> < /
+        div > <
+        div className = "user-box" >
+        <
+        input type = "password"
+        name = ""
+        required className = "input-field" / >
+        <
+        label > Password < /label> < /
+        div > <
+        a href = "#"
+        className = "submit-button" >
+        <
+        span > < /span> <
+        span > < /span> <
+        span > < /span> <
+        span > < /span>
+        Submit <
+        /a> < /
+        form > <
+        /div>
+    );
+}
+
+export default LoginForm;
